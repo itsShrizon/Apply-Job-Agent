3fc28255d65ac069aaabef283832fe831596d188
itsShrizon <99020714+itsShrizon@users.noreply.github.com>
2025-05-16 02:03:35 +0600

    Update session.py

--- apply-job-agent-backend/app/core/session.py
+++ apply-job-agent-backend/app/core/session.py
@@ -1,13 +1,11 @@
-# db/session.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Update with your PostgreSQL credentials and database name
-DATABASE_URL = 'postgresql://postgres:159@db:5432/applyjobagent'
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
